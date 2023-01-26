08324fd7339768d6fe76a257cb52c660dd733168
sanket <sanketsabale9767@gmail.com>
2023-01-26 23:01:07 +0530

    Basic Patterns are done

--- 01Basics/Patterns.java
+++ 01Basics/Patterns.java
@@ -4,6 +4,118 @@
 public class Patterns {
 
     public static void main(String[] args) {
-        System.out.println("Hello  I am Learning Patterns");
+
+        // first pattern
+        // *****
+        // *****
+        // *****
+        // *****
+
+        // Solution -
+        // for (int i = 1; i <= 4; i++) {
+        // for (int j = 1; j <= 5; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println("");
+        // }
+        // <------------------------------------------>
+        // *****
+        // * *
+        // * *
+        // *****
+
+        // Solution -
+
+        // for (int i = 1; i <= 4; i++) {
+        // for (int j = 1; j <= 5; j++) {
+        // if (i == 1 || j == 1 || i == 4 || j == 5) {
+        // System.out.print("*");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println("");
+        // }
+
+        // <------------------------------------------>
+
+        // *
+        // **
+        // ***
+        // ****
+
+        // Solution -
+
+        // for (int i = 1; i <= 4; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println("");
+        // }
+
+        // <------------------------------------------>
+
+        // ****
+        // ***
+        // **
+        // *
+
+        // Solution -
+
+        // for (int i = 4; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println("");
+        // }
+
+        // <------------------------------------------>
+
+        // *
+        // **
+        // ***
+        // ****
+
+        // for (int i = 1; i <= 4; i++) {
+        // for (int j = 1; j <= 4; j++) {
+        // if (j <= (4 - i)) {
+        // System.out.print(" ");
+        // } else {
+        // System.out.print("*");
+        // }
+        // }
+        // System.out.println("");
+        // }
+
+        // <------------------------------------------>
+
+        // 1
+        // 12
+        // 123
+        // 1234
+        // 12345
+
+        // for (int i = 1; i <= 5; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println("");
+        // }
+
+        // <------------------------------------------>
+
+        // 12345
+        // 1234
+        // 123
+        // 12
+        // 1
+
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+
     }
 }
