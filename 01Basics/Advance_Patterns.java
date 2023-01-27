9b261f33dfacb13cae1d8681d928337a148a1485
sanket <sanketsabale9767@gmail.com>
2023-01-27 21:33:53 +0530

    Working Done on Advance patterns

--- 01Basics/Advance_Patterns.java
+++ 01Basics/Advance_Patterns.java
@@ -49,15 +49,15 @@ public class Advance_Patterns {
         // *****
         // *****
         // *****
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= (5 - i); j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= 5; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= (5 - i); j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= 5; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
 
     }
 }
