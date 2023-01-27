3fa953e937f96a1cc72a3e5a5889cf480d0a9a7c
sanket <sanketsabale9767@gmail.com>
2023-01-27 09:38:39 +0530

    Understanding Advance Patterns

---
+++ 01Basics/Advance_Patterns.java
@@ -0,0 +1,63 @@
+public class Advance_Patterns {
+    public static void main(String[] args) {
+
+        // * *
+        // ** **
+        // *** ***
+        // ********
+        // ********
+        // *** ***
+        // ** **
+        // * *
+        // int n = 4;
+        // upper part
+        // for (int i = 1; i <= n; i++) {
+        // // fist part
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+
+        // // space part
+        // for (int j = 1; j <= 2 * (n - i); j++) {
+        // System.out.print(" ");
+        // }
+
+        // // 2nd Part
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println("");
+        // }
+
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // for (int j = 1; j <= 2 * (n - i); j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println("");
+        // }
+
+        // <-------------------------------------------------------------->
+
+        // *****
+        // *****
+        // *****
+        // *****
+        // *****
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= (5 - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
--- 01Basics/Patterns.java
+++ 01Basics/Patterns.java
@@ -110,9 +110,45 @@ public class Patterns {
         // 12
         // 1
 
-        for (int i = 5; i >= 1; i--) {
+        // for (int i = 5; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println("");
+        // }
+
+        // <------------------------------------------>
+
+        // 1
+        // 2 3
+        // 4 5 6
+        // 7 8 9 10
+        // 11 12 13 14 15
+
+        int val = 1;
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(val + " ");
+                val++;
+            }
+            System.out.println("");
+        }
+
+        // <------------------------------------------>
+
+        // 1
+        // 0 1
+        // 0 1 0
+        // 1 0 1 0
+        // 1 0 1 0 1
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
             }
             System.out.println("");
         }
